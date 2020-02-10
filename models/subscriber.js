3af279475bc4402072237007e59acfac14df5212
Hind Anwar <hendanwar27@outlook.com>
2020-02-11 01:36:50 +0200

    Commit

---
+++ models/subscriber.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const subscriberSchema = new mongoose.Schema({
+    name :{
+        type : String ,
+        required :true ,
+        
+    } ,
+    SubscriberToChancel:{
+        type : String ,
+        required :true ,
+      
+    } ,
+    subscribeData :{
+        type : Date ,
+        required :true ,
+        default : Date.now
+    }
+})
+
+
+module.exports =mongoose.model('Subscriber' , subscriberSchema)
